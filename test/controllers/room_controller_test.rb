4d28d5082c326dd51e85c693c334a965bad5bd67
Bhavya Bansal <bbansal@ncsu.edu>
2016-10-05 21:35:11 -0400

    test controller for room updated

--- test/controllers/room_controller_test.rb
+++ test/controllers/room_controller_test.rb
@@ -1,8 +1,28 @@
 require 'test_helper'
 
 class RoomControllerTest < ActionController::TestCase
+
+  setup do
+    @room = room(:one)
+  end
+
   test "should get index" do
     get :index
+    assert_response :success
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create room" do
+    post :create, room: {room_number: "101", building: "hill", size: "6"}
+    assert_response :success
+  end
+
+  test "should destroy room" do
+    delete :destroy, id: @room
     assert_response :success
   end
 
